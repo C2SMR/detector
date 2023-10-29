42499619d067eada85d8a85d81c009cf8f03b953
dalet <victordalet@protonmail.com>
2023-10-29 16:06:50 +0100

    feat(Navigation): set yt coockie and fullscreen page

--- src/capture/chrome/main.py
+++ src/capture/chrome/main.py
@@ -12,6 +12,15 @@ class Navigation:
         self.option.add_argument('--no-sandbox')
         self.driver = uc.Chrome(options=self.option)
         self.driver.get(self.url)
+        self.accept_cookie()
+        self.full_screen()
+
+    def accept_cookie(self):
+        self.driver.execute_script('document.cookie = "SOCS=CAESEwgDEgk1NzY3NTAwMzcaAmZyIAEaBgiAwfapBg"')
+        self.driver.execute_script('document.location.reload()')
+
+    def full_screen(self):
+        self.driver.execute_script("document.querySelector('.ytp-fullscreen-button').click()")
 
     def screen_shot(self) -> None:
         self.driver.save_screenshot('pictures/picture.png')
